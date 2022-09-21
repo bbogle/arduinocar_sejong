f8def348c5ede6040148f1815b04d6abe6ac26a5
bbogle <lbboglel@gmail.com>
2022-09-21 13:39:11 +0900

    Update auto_car_pwm.ino

--- auto_car_pwm/auto_car_pwm.ino
+++ auto_car_pwm/auto_car_pwm.ino
@@ -2,8 +2,6 @@
 //초음파센서로 거리 측정중 장애물과 거리가 20cm 이내면 1초 정지 후
 //랜덤으로 좌회전 또는 우회전 하고 다시 전진
 
-
-
 //모터 PIN 설정
 #define Motor_A1 9 //왼쪽 바퀴_빨
 #define Motor_A2 10 //왼쪽 바퀴_검
@@ -15,7 +13,8 @@
 #define IR_ECHO 6 //ECHO 핀 설정 (반사된 초음파 받는 핀)
 
 long duration = 0;
-int  speed = 150 ;   // speed :  0 ~ 255, 속도변경시 speed 값만 변경하세요. 
+int Lspeed = 170; // 좌측 모터 속도
+int Rspeed = 200; // 우측 모터 속도
                                                                                                                           
 void setup() {
   Serial.begin(9600);   //시리얼모니터
@@ -43,8 +42,8 @@ void loop() {
   //distance = ((float)(340 * duration) / 10000) / 2;
   long distance = ( duration * 340 ) / 2 / 10000;
 
-//  Serial.print("\nDistance : ");
-//  Serial.print(distance);
+  Serial.print("\nDistance : ");
+  Serial.print(distance);
 
   if (distance < 20) {        //장애물 감지 (20cm 이내)
       analogWrite(Motor_A1, 0);
@@ -53,36 +52,34 @@ void loop() {
       digitalWrite(Motor_B2, 0);
       delay(300);
       digitalWrite(Motor_A1, 0);
-      digitalWrite(Motor_A2, speed);
+      digitalWrite(Motor_A2, Lspeed);
       digitalWrite(Motor_B1, 0);
-      digitalWrite(Motor_B2, speed);  
+      digitalWrite(Motor_B2, Rspeed);  
       delay(300);
     
     int rnd = random(0, 2);  //장애물 감지시 좌/우회전 랜덤처리
     if (rnd == 0) {
-      int t = random(1,4); // 랜덤
       Serial.print("Left, ");
-      analogWrite(Motor_A1, speed);
+      analogWrite(Motor_A1, 0);
+      analogWrite(Motor_A2, 0);
+      analogWrite(Motor_B1, Rspeed);
+      analogWrite(Motor_B2, 0);
+      delay(400);
+    }
+    else {
+      Serial.print("Right, ");
+      analogWrite(Motor_A1, Lspeed);
       analogWrite(Motor_A2, 0);
       analogWrite(Motor_B1, 0);
       analogWrite(Motor_B2, 0);
-      delay(t*150);
-    }
-    else {
-      int t = random(1,4); // 랜덤
-      Serial.print("Right, ");
-      analogWrite(Motor_A1, 0);
-      analogWrite(Motor_A2, 0);
-      analogWrite(Motor_B1, speed);
-      analogWrite(Motor_B2, 0);
-      delay(t*150);
+      delay(400);
     }
   }
   else {        //장애물 없음
     Serial.println(" forward");
-    analogWrite(Motor_A1, speed);
+    analogWrite(Motor_A1, Lspeed);
     analogWrite(Motor_A2, 0);
-    analogWrite(Motor_B1, speed);
+    analogWrite(Motor_B1, Rspeed);
     analogWrite(Motor_B2, 0);
   }
 }
